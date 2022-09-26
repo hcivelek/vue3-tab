e46981ad87dbd683b2bb27e908f0a68b86c0a4f1
Hasan Civelek <hasan@kodventure.com>
2022-09-26 04:33:12 +0300

    Create BTabs.vue

---
+++ BTabs.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref, provide } from 'vue'
+
+const tabProvider = ref({
+    active: 0,
+    count: 0 // every slot will increase this and set it as its own index.
+})
+
+provide('tabProvider', tabProvider)
+
+</script>
+<template>
+    <ul class="nav nav-tabs">
+        <slot />
+    </ul>
+</template>
